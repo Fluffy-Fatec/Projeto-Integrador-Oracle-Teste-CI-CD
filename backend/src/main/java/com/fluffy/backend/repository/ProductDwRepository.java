ccfff52770680bc4f3e95985f71a4c392adb54ed
Victor Fernandes <victor.santos136@fatec.sp.gov.br>
2023-11-11 19:43:02 -0300

    banco

--- backend/src/main/java/com/fluffy/backend/repository/ProductDwRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.fluffy.backend.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import com.fluffy.backend.entity.Product;
-//
-//public interface ProductDwRepository extends JpaRepository<ProductDW, Integer>{
-//
-//}
--- backend/src/test/java/com/fluffy/backend/service/SupplierStockOfferServiceTest.java
+++ backend/src/test/java/com/fluffy/backend/service/SupplierStockOfferServiceTest.java
@@ -1,6 +1,7 @@
 package com.fluffy.backend.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
@@ -12,9 +13,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import com.fluffy.backend.DTO.SupplierStockOfferDTO;
 import com.fluffy.backend.entity.Stocks;
@@ -38,59 +42,65 @@ public class SupplierStockOfferServiceTest {
 	@Mock
 	private StocksRepository stocksRepository;
 
+	@BeforeEach
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+	}
+
 	@Test
 	public void testCreateSupplierStockOffer() {
-		Long supplierId = 1L;
-		Long stockId = 2L;
+	    Long supplierId = 1L;
+	    Long stockId = 2L;
 
-		SupplierStockOfferDTO supplierStockOfferDTO = new SupplierStockOfferDTO();
-		supplierStockOfferDTO.setSupplierId(supplierId);
-		supplierStockOfferDTO.setStockId(stockId);
-		supplierStockOfferDTO.setQuantityCan(100.0);
-		supplierStockOfferDTO.setMeasurement("Kg");
-		supplierStockOfferDTO.setValue(new BigDecimal("50.0"));
+	    SupplierStockOfferDTO supplierStockOfferDTO = new SupplierStockOfferDTO();
+	    supplierStockOfferDTO.setSupplierId(supplierId);
+	    supplierStockOfferDTO.setStockId(stockId);
+	    supplierStockOfferDTO.setQuantityCan(50.0);
+	    supplierStockOfferDTO.setMeasurement("Kg");
+	    supplierStockOfferDTO.setValue(new BigDecimal("50.0"));
 
-		Suppliers supplier = new Suppliers();
-		supplier.setId(supplierId);
+	    Suppliers supplier = new Suppliers();
+	    supplier.setId(supplierId);
 
-		Stocks stock = new Stocks();
-		stock.setIdstock(stockId);
+	    Stocks stock = new Stocks();
+	    stock.setIdstock(stockId);
 
-		when(suppliersRepository.findById(supplierId)).thenReturn(Optional.of(supplier));
-		when(stocksRepository.findById(stockId)).thenReturn(Optional.of(stock));
+	    when(suppliersRepository.findById(supplierId)).thenReturn(Optional.of(supplier));
+	    when(stocksRepository.findById(stockId)).thenReturn(Optional.of(stock));
 
-		SupplierStockOffer newSupplierStockOffer = new SupplierStockOffer();
-		newSupplierStockOffer.setSuppliers(supplier);
-		newSupplierStockOffer.setStocks(stock);
-		newSupplierStockOffer.setQuantityCan(100.0);
-		newSupplierStockOffer.setMeasurement("Kg");
-		newSupplierStockOffer.setValue(new BigDecimal("50.0"));
+	    SupplierStockOffer newSupplierStockOffer = new SupplierStockOffer();
+	    newSupplierStockOffer.setSuppliers(supplier);
+	    newSupplierStockOffer.setStocks(stock);
+	    newSupplierStockOffer.setQuantityCan(50.0); // Ajuste aqui
+	    newSupplierStockOffer.setMeasurement("Kg");
+	    newSupplierStockOffer.setValue(new BigDecimal("50.0"));
 
-		when(supplierStockOfferRepository.save(any(SupplierStockOffer.class))).thenReturn(newSupplierStockOffer);
+	    when(supplierStockOfferRepository.save(any(SupplierStockOffer.class))).thenReturn(newSupplierStockOffer);
 
-		SupplierStockOffer result = supplierStockOfferService.createSupplierStockOffer(supplierStockOfferDTO);
+	    SupplierStockOffer result = supplierStockOfferService.createSupplierStockOffer(supplierStockOfferDTO);
+
+	    assertNotNull(result);
+	    assertEquals(supplier, result.getSuppliers());
+	    assertEquals(stock, result.getStocks());
+	    assertEquals(50.0, result.getQuantityCan(), 0.0); // 0.0 é a margem de erro aceitável
+	    assertEquals("Kg", result.getMeasurement());
+	    assertEquals(new BigDecimal("50.0"), result.getValue());
+	}
+
+
+	@Test
+	public void testGetAllSupplierStockOffer() {
+		List<SupplierStockOffer> supplierStockOffers = new ArrayList<>();
+		supplierStockOffers.add(new SupplierStockOffer());
+		supplierStockOffers.add(new SupplierStockOffer());
+
+		when(supplierStockOfferRepository.findAll()).thenReturn(supplierStockOffers);
+
+		List<SupplierStockOffer> result = supplierStockOfferService.getAllSupplierStockOffer();
 
 		assertNotNull(result);
-		assertEquals(supplier, result.getSuppliers());
-		assertEquals(stock, result.getStocks());
-		assertEquals(new BigDecimal("100.0"), result.getQuantityCan());
-		assertEquals("Kg", result.getMeasurement());
-		assertEquals(new BigDecimal("50.0"), result.getValue());
+		assertEquals(supplierStockOffers.size(), result.size());
 	}
-	
-	 @Test
-	    public void testGetAllSupplierStockOffer() {
-	        List<SupplierStockOffer> supplierStockOffers = new ArrayList<>();
-	        supplierStockOffers.add(new SupplierStockOffer());
-	        supplierStockOffers.add(new SupplierStockOffer());
-
-	        when(supplierStockOfferRepository.findAll()).thenReturn(supplierStockOffers);
-
-	        List<SupplierStockOffer> result = supplierStockOfferService.getAllSupplierStockOffer();
-
-	        assertNotNull(result);
-	        assertEquals(supplierStockOffers.size(), result.size());
-	    }
 
 	@Test
 	public void testDeleteSupplierStockOfferPorId() {
